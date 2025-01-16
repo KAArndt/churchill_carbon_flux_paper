eb1985325c0269e75534fb2fa221cd7f6e469749
KAArndt <62759950+KAArndt@users.noreply.github.com>
2025-01-16 13:15:24 -0500

    light response edits

--- light response.R
+++ light response.R
@@ -12,7 +12,7 @@ df$dou
 
 library(bigleaf)
 
-#different averaging methods
+#different averaging methods ANCflux tester
 library(dplyr)
 df$month = format(df$date,'%m')
 df$doy   = format(df$date,'%j')
@@ -59,15 +59,14 @@ fin = merge(dmo,hh,by = c('month','year'),all = T)
 fin = merge(fin,mo,by = c('month','year'),all = T)
 
 summary(lm())
-
-
-
+############################################################################################
 av = timeAverage(mydata = df,avg.time = '1 day',data.thresh = 50,statistic = 'mean')
 gs = subset(av,av$TA > 5 & av$PPFD_IN > 0)
 gs = gs[complete.cases(gs$PPFD_IN),]
 gs = gs[complete.cases(gs$FC),]
 
-gs = subset(df,df$TA > 8 & df$PPFD_IN > 0)
+# growing season control based on light response curve #######################
+gs = subset(df,df$TS_3_3_1 > 10 & df$PPFD_IN > 1)
 gs = gs[complete.cases(gs$PPFD_IN),]
 gs = gs[complete.cases(gs$FC),]
 
@@ -75,30 +74,67 @@ lr = light.response(data = gs,NEE = 'FC',Reco = 'RECO_PI',PPFD = 'PPFD_IN',PPFD_
 lr
 fit = lr$m$fitted()
 
-
-ggplot(data = gs)+
-  geom_point(aes(PPFD_IN,GPP_PI_F,col=TA))+
-  geom_line(aes(PPFD_IN,fit),col='red')
-
 ggplot(data = gs)+
   geom_point(aes(PPFD_IN,FC,col=TA))+
-  geom_line(aes(PPFD_IN,-fit),col='red')
-
-gs = df[complete.cases(df$PPFD_IN),]
-gs = gs[complete.cases(gs$FC),]
-
-lr = light.response(data = gs,NEE = 'FC',Reco = 'RECO_PI',PPFD = 'PPFD_IN',PPFD_ref = 1500)
-lr
-fit = lr$m$fitted()
+  geom_line(aes(PPFD_IN,fit*-1),col='red')
 
 
-ggplot(data = df)+
-  geom_point(aes(PPFD_IN,GPP_PI_F,col=TA))+
-  geom_line(aes(PPFD_IN,fit),col='red')
+ggplot(data = gs,aes(fit*-1,FC))+
+  geom_point()+
+  geom_smooth(method = 'lm')
 
-ggplot(data = gs)+
-  geom_point(aes(PPFD_IN,FC,col=TA))+
-  geom_line(aes(PPFD_IN,-fit),col='red')
+lr.mod = lm(gs$FC ~ fit*-1)
+summary(lr.mod)
+
+##########################
+df$doy = as.numeric(format(df$date,'%j'))
+av = timeAverage(mydata = df,avg.time = '1 day',data.thresh = 50,statistic = 'mean')
+shs = subset(av,av$TS_3_4_1 < 1 | av$PPFD_IN < 100)
+shs = subset(df,df$TS_3_4_1 < 1 | df$PPFD_IN < 25)
+#shs = subset(df,df$TS_3_4_1 < 1)
+
+# shs = shs[complete.cases(shs$TS_3_4_1),]
+# shs = shs[complete.cases(shs$FC),]
+
+
+ggplot(data = shs)+
+  geom_point(aes(TA,FC,col=doy))
+
+
+av = timeAverage(mydata = shs,avg.time = '1 day',data.thresh = 50,statistic = 'mean')
+
+ggplot(data = subset(av,av$FC>0))+
+  geom_point(aes(TA,log10(FC),col=doy))+
+  geom_smooth()
+
+t.mod = lm(log(shs$FC) ~ shs$TS_3_1_1)
+summary(t.mod)
+
+ggplot(data = shs)+
+  geom_point(aes(TA,FC,col=PPFD_IN))
+ggplot(data = shs)+
+  geom_point(aes(TA,FC,col=doy))
+
+ggplot(data = shs,aes(fit*-1,FC))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+
+
+
+
+
+
+
+df2 = subset(df,df$PPFD_IN > 1 & df$TS_3_3_1 > 5)
+
+ggplot(data = df2,aes(PPFD_IN,FC,col=TS_3_3_1))+
+  geom_point()+
+  geo
+  geom_smooth(method = 'lm')
+
+summary(lm(formula = df2$FC ~ df2$PPFD_IN))
+
 
 #−NEE = αPPFD/(1−(PPFD/PPFDref)+αPPFD/GPPref)−Reco
 α = 0.007827
